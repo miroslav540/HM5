0cee5e112ecb6809f72567d2a1fd5de5451ec914
Miroslav <mir19972905@mai.ru>
2022-07-16 11:05:39 +0300

    task43

---
+++ task43/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+void findCoordinates (double k1, double k2, double b1, double b2)
+{
+    double x = (b1 - b2)/(k2 - k1);
+    double y = x * k1 + b1;
+    Console.WriteLine("{0} , {1}", x , y);
+}
+
+findCoordinates(5,9,2,4);
