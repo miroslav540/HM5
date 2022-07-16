07c14a3d351ac4cbb8611650492528ffe58ac007
Miroslav <mir19972905@mai.ru>
2022-07-16 11:05:30 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,12 @@
+﻿int M = Convert.ToInt32(Console.ReadLine());
+int howManyPositives = 0;
+for(int i = 0; i< M; i++)
+{
+    int digit = Convert.ToInt32(Console.ReadLine());
+    if (digit > 0)
+    {
+        howManyPositives++;
+    }
+}
+
+Console.WriteLine("Positives: {0}", howManyPositives);
