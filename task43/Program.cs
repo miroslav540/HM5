2a4f249afef22b0a86ac723a50ea857ab7bebcd9
Miroslav <mir19972905@mai.ru>
2022-07-17 22:38:31 +0300

    make some changes

--- task43/Program.cs
+++ task43/Program.cs
@@ -1,10 +1,15 @@
 ﻿
 
-void findCoordinates (double k1, double k2, double b1, double b2)
+void findCoordinates ()
 {
+    Console.WriteLine("Enter coordinates");
+    double k1 = Convert.ToInt32(Console.ReadLine());
+    double k2 = Convert.ToInt32(Console.ReadLine());
+    double b1 = Convert.ToInt32(Console.ReadLine());
+    double b2 = Convert.ToInt32(Console.ReadLine());
     double x = (b1 - b2)/(k2 - k1);
     double y = x * k1 + b1;
     Console.WriteLine("{0} , {1}", x , y);
 }
 
-findCoordinates(5,9,2,4);
+findCoordinates();
